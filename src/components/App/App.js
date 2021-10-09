79de26c1e7726170a15857d45b3d2d739469826e
jcpengy10 <66973358+jcpengy10@users.noreply.github.com>
2021-10-09 00:09:05 -0400

    created react app

---
+++ src/components/App/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Dashboard from '../Dashboard/Dashboard'; 
+import Preferences from '../Preferences/Preferences'; 
+
+function App() {
+  return (
+    <></>
+  );
+}
+
+export default App;
---
+++ src/components/Preferences/Preferences.js
@@ -0,0 +1,8 @@
+import React from 'react'; 
+
+function React() {
+    return (
+        <h2>React</h2>
+    ); 
+}
+export default React; 
